00b62d3d49d14566737fc7cf703fd7b8e55f4892
Ben <me@bpmct.net>
2020-03-13 13:32:12 -0700

    bootstrap & fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    {/* <h1>Hi people</h1> */}
+    <p>Hi! My name is Ben. I do web design and social media stuff.</p>
+    <p>
+      This is an awful professional site, but it will get better over time. Good
+      thing it's not live!
+    </p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
